29fd74cb2a4cbeefb7c4a7b881c76a7686d46cde
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-01 15:25:37 +0100

    refactor

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,22 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   createUser,
   getSingleUser,
   saveBook,
   deleteBook,
   login,
-} = require('../../controllers/user-controller');
+} = require("../../controllers/user-controller");
 
 // import middleware
-const { authMiddleware } = require('../../utils/auth');
+const { signToken } = require("../../utils/auth");
 
 // put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, saveBook);
+router.route("/").post(createUser).put(signToken, saveBook);
 
-router.route('/login').post(login);
+router.route("/login").post(login);
 
-router.route('/me').get(authMiddleware, getSingleUser);
+router.route("/me").get(signToken, getSingleUser);
 
-router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+router.route("/books/:bookId").delete(signToken, deleteBook);
 
 module.exports = router;
