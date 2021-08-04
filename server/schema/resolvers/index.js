fba4b3b949133e3b29bec802c5f60d003ff0b1f4
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-04 18:32:06 +0100

    import mutations

--- server/schema/resolvers/index.js
+++ server/schema/resolvers/index.js
@@ -1,11 +1,15 @@
 const Book = require("./book");
 const me = require("./user");
+const signup = require("./signup");
+const login = require("./login");
 
 const resolvers = {
   Query: {
     // Book,
     me,
   },
+
+  Mutation: { signup, login },
 };
 
 module.exports = resolvers;
