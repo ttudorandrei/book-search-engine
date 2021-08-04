60a7108a9b90d417c60a99b38a99a25975b8bdb5
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-04 22:48:35 +0100

    add verifyToken, refactor

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -3,11 +3,11 @@ const bcrypt = require("bcrypt");
 
 // set token secret and expiration date
 const secret = process.env.JWT_SECRET || "my local secret";
-const expiration = process.env.JWT_EXPIRES_IN || "2h";
+const expiresIn = process.env.JWT_EXPIRES_IN || "1h";
 
 // create token with payload
 const signToken = (payload) => {
-  return jwt.sign(payload, secret, { expiresIn: expiration });
+  return jwt.sign(payload, secret, { expiresIn });
 };
 
 const hashPassword = async function (next) {
@@ -21,40 +21,13 @@ const validatePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-module.exports = {
-  hashPassword,
-  validatePassword,
+const verifyToken = (token) => {
+  return jwt.verify(token, secret, { maxAge: expiresIn });
 };
 
 module.exports = {
-  // // function for our authenticated routes
-  // authMiddleware: function (req, res, next) {
-  //   // allows token to be sent via  req.query or headers
-  //   let token = req.query.token || req.headers.authorization;
-  //   // ["Bearer", "<tokenvalue>"]
-  //   if (req.headers.authorization) {
-  //     token = token.split(" ").pop().trim();
-  //   }
-  //   if (!token) {
-  //     return res.status(400).json({ message: "You have no token!" });
-  //   }
-  //   // verify token and get user data out of it
-  //   try {
-  //     const { data } = jwt.verify(token, secret, { maxAge: expiration });
-  //     req.user = data;
-  //   } catch {
-  //     console.log("Invalid token");
-  //     return res.status(400).json({ message: "invalid token!" });
-  //   }
-  //   // send to next endpoint
-  //   next();
-  // },
-  // signToken: function ({ username, email, _id }) {
-  //   const payload = { username, email, _id };
-  //   return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  // },
-
   signToken,
   hashPassword,
   validatePassword,
+  verifyToken,
 };
