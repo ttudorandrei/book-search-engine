f8791f31cd471d1829477b19d8e36ecddcde3106
Tudor Tocan <tudor.tocan@gmail.com>
2021-08-05 15:26:03 +0100

    finding user by id using findOne, id is coming from context

--- server/schema/resolvers/user.js
+++ server/schema/resolvers/user.js
@@ -1,7 +1,7 @@
 const { User } = require("../../models");
 
-const user = async () => {
-  const user = await User.findById("610ac5f666ff620464b333bc");
+const user = async (_, args, context) => {
+  const user = await User.findOne({ _id: context.user.id });
 
   return user;
 };
